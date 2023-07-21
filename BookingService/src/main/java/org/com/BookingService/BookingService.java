0a3586658a5f79eab23abd0620631de0e2480e05
PHoaiLe <lehoaiphuong13042002@gmail.com>
2023-07-21 17:15:13 +0700

    "add BookingService"

---
+++ BookingService/src/main/java/org/com/BookingService/BookingService.java
@@ -0,0 +1,13 @@
+package org.com.BookingService;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class BookingService
+{
+    public static void main(String[] args) {
+        SpringApplication.run(BookingService.class, args);
+    }
+}
